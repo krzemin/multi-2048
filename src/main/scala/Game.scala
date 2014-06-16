3e977ecb0c6ed63d40d4cfea52a33686a8932e5b
Piotr Krzemiński <pio.krzeminski@gmail.com>
2014-06-17 00:02:54 +0200

    moving in games; test for simple status checking

--- src/main/scala/Game.scala
+++ src/main/scala/Game.scala
@@ -4,6 +4,20 @@ class Game(size: Int) { self: Board =>
   var board2 = addRandomField(newBoard(size))
 
   import Game.Status._
+  import Board.Move._
+
+  def move(move: Move): Boolean = {
+    val nextBoards = for {
+      m1 <- performMove(move, board1)
+      m2 <- performMove(move, board2)
+    } yield (m1, m2)
+
+    nextBoards.fold(false)({ case (b1, b2) =>
+      board1 = b1
+      board2 = b2
+      true
+    })
+  }
 
   def status: Status = (isBoardStuck(board1), isBoardStuck(board2)) match {
     case (false, false) => InProgress
--- src/test/scala/GameSpec.scala
+++ src/test/scala/GameSpec.scala
@@ -5,9 +5,13 @@ class GameSpec extends Specification {
   import Game.Status._
 
   "Game" should {
-    val game = new Game(4) with Board with Transformation2048 with RandomGenFake
     "claim in-progress status for new game" in {
+      val game = new Game(4) with Board with Transformation2048 with RandomGenFake
       game.status === InProgress
+    }
+    "claim new 1x1 size game to be draw" in {
+      val game = new Game(1) with Board with Transformation2048 with RandomGenFake
+      game.status === Draw
     }
   }
 
