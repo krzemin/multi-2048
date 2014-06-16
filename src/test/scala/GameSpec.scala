de8c11ae5214ace6bc2f845db3b9a1d654ff0cf4
Piotr Krzemiński <pio.krzeminski@gmail.com>
2014-06-16 16:28:47 +0200

    tests for board functions

--- src/test/scala/GameSpec.scala
+++ src/test/scala/GameSpec.scala
@@ -7,11 +7,25 @@ class GameSpec extends Specification {
   "emptyBoard" should {
     val board = emptyBoard(5)
     "create board with specified size" in {
-      board.size must beEqualTo(5)
-      board.head.size must beEqualTo(5)
+      board.size === 5
+      board.forall(_.size === 5)
     }
     "create board filled with empty fields" in {
-      board.forall(_.forall(_ == None)) must beTrue
+      board.forall(_.forall(_ === None))
+    }
+  }
+
+  "isEmpty" should {
+    val board = emptyBoard(5)
+    "be true on empty board" in {
+      for {
+        x <- 0 until 5
+        y <- 0 until 5
+      } isEmpty(x, y)(board) === true
+    }
+    "be false on occupied fields" in {
+      val board1 = putBoard(3, 4, 1)(board)
+      isEmpty(3, 4)(board) === false
     }
   }
 
