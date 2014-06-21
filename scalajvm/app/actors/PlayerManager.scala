753bf474bae614908ef42fff3b955b68486834e7
Piotr Krzeminski <pio.krzeminski@gmail.com>
2014-06-21 21:15:53 +0200

    renaming

--- scalajvm/app/actors/PlayerManager.scala
+++ scalajvm/app/actors/PlayerManager.scala
@@ -4,15 +4,14 @@ import akka.actor.{Props, ActorRef, Actor}
 import game2048.Board
 
 
-object EchoActor {
-  def props(out: ActorRef) = Props(new EchoActor(out))
+object PlayerManager {
+  def props(out: ActorRef) = Props(new PlayerManager(out))
 }
 
-class EchoActor(out: ActorRef) extends Actor {
+class PlayerManager(out: ActorRef) extends Actor {
 
   def receive: Receive = {
-    case TestValue(v) =>
-      out ! TestValue(v)
+    case WantPlayHuman =>
       out ! NewGame(4, Board(4).put(2,2,2), Board(4).put(3,3,2), false)
   }
 }
